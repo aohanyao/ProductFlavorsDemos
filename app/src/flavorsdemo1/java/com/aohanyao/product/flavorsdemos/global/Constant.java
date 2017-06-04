8e1e6e2e3ea05ecc86b034000f648f17694d6013
江俊超 <928692385@qq.com>
2017-06-04 23:13:03 +0800

    变种配置，源码配置

---
+++ app/src/flavorsdemo1/java/com/aohanyao/product/flavorsdemos/global/Constant.java
@@ -0,0 +1,9 @@
+package com.aohanyao.product.flavorsdemos.global;
+
+/**
+ * Created by jjc on 2017/6/4.
+ */
+
+public class Constant {
+    public static final String API_ADDRESS="这是变种1的API地址";
+}
--- app/src/main/java/com/aohanyao/product/flavorsdemos/MainActivity.java
+++ app/src/main/java/com/aohanyao/product/flavorsdemos/MainActivity.java
@@ -2,12 +2,19 @@ package com.aohanyao.product.flavorsdemos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.aohanyao.product.flavorsdemos.global.Constant;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvApi = (TextView) findViewById(R.id.tv_api);
+        tvApi.setText(Constant.API_ADDRESS);
     }
 }
